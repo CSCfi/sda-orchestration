550a04b4268b7c682cfe413aecff08df6c3d9c52
Stefan Negru <stefan@blankdots.com>
2020-09-17 21:57:18 +0300

    add env variables for db connection

--- sda_orchestrator/utils/db_ops.py
+++ sda_orchestrator/utils/db_ops.py
@@ -7,18 +7,21 @@ from pathlib import Path
 
 def map_file2dataset(user, filepath, decrypted_checksum, dataset_id):
     """Assign file to dataset, for dataset driven permissions."""
-    conn = psycopg2.connect(user='lega_in',
-                            password=os.environ.get('DB_LEGA_IN_PASSWORD'),
-                            database='lega',
-                            host=os.environ.get('DB_HOST', 'localhost'),
-                            sslmode='require',
-                            sslrootcert=Path("/tls/certs/root.ca.crt"),
-                            sslcert=Path("/tls/certs/cert.ca.crt"),
-                            sslkey=Path("/tls/certs/cert.ca.key"))
+    conn = psycopg2.connect(
+        user=os.environ.get("DB_IN_USER", "lega_in"),
+        password=os.environ.get("DB_IN_PASSWORD"),
+        database=os.environ.get("DB_NAME", "lega"),
+        host=os.environ.get("DB_HOST", "localhost"),
+        sslmode="require",
+        sslrootcert=Path("/tls/certs/root.ca.crt"),
+        sslcert=Path("/tls/certs/cert.ca.crt"),
+        sslkey=Path("/tls/certs/cert.ca.key"),
+    )
     with conn.cursor() as cursor:
-        cursor.execute('SELECT status FROM local_ega.files where elixir_id = %(user)s AND inbox_path = %(filepath)s',
-                       {"user": user,
-                        "filepath": filepath})
+        cursor.execute(
+            "SELECT status FROM local_ega.files where elixir_id = %(user)s AND inbox_path = %(filepath)s",
+            {"user": user, "filepath": filepath},
+        )
         files = cursor.fetchall()
         # wait for file satus to be ready
         sleep_time = 2
@@ -26,8 +29,8 @@ def map_file2dataset(user, filepath, decrypted_checksum, dataset_id):
         for x in range(0, num_retries):
             try:
                 for f in files:
-                    if f[0] != 'READY' and f[0] != 'DISABLED':
-                        LOG.info(f'trying {f[0]}')
+                    if f[0] != "READY" and f[0] != "DISABLED":
+                        LOG.info(f"trying {f[0]}")
                         raise Exception
             except Exception as str_error:
                 if str_error:
@@ -37,34 +40,42 @@ def map_file2dataset(user, filepath, decrypted_checksum, dataset_id):
                     break
 
     with conn.cursor() as cursor:
-        cursor.execute('SELECT id FROM local_ega.files where elixir_id = %(user)s AND inbox_path = %(filepath)s'
-                       ' AND archive_file_checksum = %(decrypted_checksum)s',
-                       {"user": user,
-                        "filepath": filepath,
-                        "decrypted_checksum": decrypted_checksum})
+        cursor.execute(
+            "SELECT id FROM local_ega.files where elixir_id = %(user)s AND inbox_path = %(filepath)s"
+            " AND archive_file_checksum = %(decrypted_checksum)s",
+            {"user": user, "filepath": filepath, "decrypted_checksum": decrypted_checksum},
+        )
         files = cursor.fetchall()
     conn.close()
 
     last_index = None
     # table in data out requires a different user, thus also a different connection
-    conn2 = psycopg2.connect(user='lega_out',
-                             password=os.environ.get('DB_LEGA_OUT_PASSWORD'),
-                             database='lega',
-                             host=os.environ.get('DB_HOST', 'localhost'),
-                             sslmode='require',
-                             sslrootcert=Path("/tls/certs/root.ca.crt"),
-                             sslcert=Path("/tls/certs/cert.ca.crt"),
-                             sslkey=Path("/tls/certs/cert.ca.key"))
+    conn2 = psycopg2.connect(
+        user=os.environ.get("DB_OUT_USER", "lega_out"),
+        password=os.environ.get("DB_OUT_PASSWORD"),
+        database=os.environ.get("DB_NAME", "lega"),
+        host=os.environ.get("DB_HOST", "localhost"),
+        sslmode="require",
+        sslrootcert=Path("/tls/certs/root.ca.crt"),
+        sslcert=Path("/tls/certs/cert.ca.crt"),
+        sslkey=Path("/tls/certs/cert.ca.key"),
+    )
 
     with conn2.cursor() as cursor:
-        cursor.execute('''SELECT id FROM local_ega_ebi.filedataset ORDER BY id DESC LIMIT 1''')
+        cursor.execute("""SELECT id FROM local_ega_ebi.filedataset ORDER BY id DESC LIMIT 1""")
         value = cursor.fetchone()
         last_index = value[0] if value is not None else 0
 
     with conn2.cursor() as cursor:
         for f in files:
-            cursor.execute('INSERT INTO local_ega_ebi.filedataset(id, file_id, dataset_stable_id) VALUES(%(last_index)s, %(file_id)s, %(dataset_id)s)',
-                           {"last_index": last_index + 1 if last_index is not None else 1, "file_id": f[0], "dataset_id": dataset_id})
+            cursor.execute(
+                "INSERT INTO local_ega_ebi.filedataset(id, file_id, dataset_stable_id) VALUES(%(last_index)s, %(file_id)s, %(dataset_id)s)",
+                {
+                    "last_index": last_index + 1 if last_index is not None else 1,
+                    "file_id": f[0],
+                    "dataset_id": dataset_id,
+                },
+            )
             last_index += 1
             LOG.debug(f"Mapped ID: {f[0]} to Dataset: {dataset_id}")
             conn2.commit()
@@ -73,14 +84,16 @@ def map_file2dataset(user, filepath, decrypted_checksum, dataset_id):
 
 def retrieve_file2dataset():
     """Assign file to dataset, for dataset driven permissions."""
-    conn = psycopg2.connect(user='lega_out',
-                            password=os.environ.get('DB_LEGA_OUT_PASSWORD'),
-                            database='lega',
-                            host=os.environ.get('DB_HOST', 'localhost'),
-                            sslmode='require',
-                            sslrootcert=Path("/tls/certs/root.ca.crt"),
-                            sslcert=Path("/tls/certs/cert.ca.crt"),
-                            sslkey=Path("/tls/certs/cert.ca.key"))
+    conn = psycopg2.connect(
+        user=os.environ.get("DB_OUT_USER", "lega_out"),
+        password=os.environ.get("DB_OUT_PASSWORD"),
+        database=os.environ.get("DB_NAME", "lega"),
+        host=os.environ.get("DB_HOST", "localhost"),
+        sslmode="require",
+        sslrootcert=Path("/tls/certs/root.ca.crt"),
+        sslcert=Path("/tls/certs/cert.ca.crt"),
+        sslkey=Path("/tls/certs/cert.ca.key"),
+    )
     with conn.cursor() as cursor:
         cursor.execute("SELECT file_id, dataset_id FROM local_ega_ebi.file_dataset WHERE file_id is NOT NULL")
         matches = cursor.fetchall()
