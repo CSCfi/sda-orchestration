219e7a3a71e4771d9cae90b5a5aff826cbd39c31
Stefan Negru <stefan@blankdots.com>
2020-10-05 19:35:17 +0300

    test consumers can be called

--- tests/test_consumers.py
+++ tests/test_consumers.py
@@ -2,10 +2,12 @@
 
 import unittest
 from unittest.mock import patch
-from sda_orchestrator.complete_consume import main
+from sda_orchestrator.inbox_consume import main as inbox_main
+from sda_orchestrator.complete_consume import main as complete_main
+from sda_orchestrator.verified_consume import main as verified_main
 
 
-class CompleteConsumerCalled(unittest.TestCase):
+class ConsumersCalled(unittest.TestCase):
     """Test for Messaging."""
 
     def setUp(self):
@@ -15,5 +17,19 @@ class CompleteConsumerCalled(unittest.TestCase):
     @patch("sda_orchestrator.complete_consume.CompleteConsumer")
     def test_start_complete_consumer(self, mock):
         """Test if start a consumer was called."""
-        main()
+        complete_main()
+        self.assertTrue(mock.called)
+
+    @patch("amqpstorm.Connection")
+    @patch("sda_orchestrator.inbox_consume.InboxConsumer")
+    def test_start_inbox_consumer(self, mock, amqp_mock):
+        """Test if start a consumer was called."""
+        inbox_main()
+        self.assertTrue(mock.called)
+
+    @patch("amqpstorm.Connection")
+    @patch("sda_orchestrator.verified_consume.VerifyConsumer")
+    def test_start_verified_consumer(self, mock, amqp_mock):
+        """Test if start a consumer was called."""
+        verified_main()
         self.assertTrue(mock.called)
