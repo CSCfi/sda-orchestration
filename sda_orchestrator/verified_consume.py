89ffb5a75eb6c18d5dfce1606a3eb26d7bd9312f
Stefan Negru <stefan@blankdots.com>
2020-11-24 14:01:06 +0200

    verified just generates simple IDs and publishes to accessionids
    STABLEIDS_QUEUE -> ACCESSIONIDS_QUEUE

--- sda_orchestrator/verified_consume.py
+++ sda_orchestrator/verified_consume.py
@@ -5,7 +5,6 @@ from .utils.consumer import Consumer
 from .utils.logger import LOG
 import os
 from .utils.id_ops import generate_accession_id
-from .utils.db_ops import map_file2dataset
 
 
 class VerifyConsumer(Consumer):
@@ -32,20 +31,19 @@ class VerifyConsumer(Consumer):
                 "decrypted_checksums": cmp_msg["decrypted_checksums"],
                 "accession_id": accessionID,
             }
-            sent = Message.create(channel, json.dumps(content), properties)
+            accession = Message.create(channel, json.dumps(content), properties)
             checksum_data = list(filter(lambda x: x["type"] == "sha256", cmp_msg["decrypted_checksums"]))
             decrypted_checksum = checksum_data[0]["value"]
-            sent.publish(
-                os.environ.get("STABLEIDS_QUEUE", "stableIDs"), exchange=os.environ.get("BROKER_EXCHANGE", "localega")
+            accession.publish(
+                os.environ.get("ACCESSIONIDS_QUEUE", "accessionIDs"), exchange=os.environ.get("BROKER_EXCHANGE", "sda")
             )
 
             channel.close()
             LOG.info(
-                f'Sent the message to stabled IDs queue to get accession ID for file {cmp_msg["filepath"]} \
+                f'Sent the message to accessionIDs queue to set accession ID for file {cmp_msg["filepath"]} \
                      with checksum {decrypted_checksum}.'
             )
-
-            map_file2dataset(cmp_msg["user"], cmp_msg["filepath"], decrypted_checksum)
+            
 
         except Exception as error:
             LOG.error("Something went wrong: {0}".format(error))
@@ -56,10 +54,10 @@ def main() -> None:
     CONSUMER = VerifyConsumer(
         hostname=str(os.environ.get("BROKER_HOST")),
         port=int(os.environ.get("BROKER_PORT", 5670)),
-        username=os.environ.get("BROKER_USER", "lega"),
+        username=os.environ.get("BROKER_USER", "sda"),
         password=os.environ.get("BROKER_PASSWORD", ""),
-        queue=os.environ.get("VERIFIED_QUEUE", "files.verified"),
-        vhost=os.environ.get("BROKER_VHOST", "lega"),
+        queue=os.environ.get("VERIFIED_QUEUE", "verified"),
+        vhost=os.environ.get("BROKER_VHOST", "sda"),
     )
     CONSUMER.start()
 
