a8616952be7211dbf4c1e907c044618cda289290
Stefan Negru <stefan@blankdots.com>
2020-12-08 22:49:50 +0200

    validate complete consumer incoming & outgoing msg

--- sda_orchestrator/complete_consume.py
+++ sda_orchestrator/complete_consume.py
@@ -5,6 +5,8 @@ from .utils.consumer import Consumer
 from .utils.logger import LOG
 import os
 from .utils.id_ops import generate_dataset_id
+from jsonschema.exceptions import ValidationError
+from .schemas.validate import ValidateJSON, load_schema
 
 
 class CompleteConsumer(Consumer):
@@ -15,35 +17,60 @@ class CompleteConsumer(Consumer):
         try:
             complete_msg = json.loads(message.body)
 
-            LOG.info(f"Completed message received: {complete_msg} .")
-            properties = {
-                "content_type": "application/json",
-                "headers": {},
-                "correlation_id": message.correlation_id,
-                "delivery_mode": 2,
-            }
+            LOG.debug(f"MQ Message body: {message.body} .")
+            LOG.debug(f"Complete Consumer message received: {complete_msg} .")
+            LOG.info(
+                f"Received work (corr-id: {message.correlation_id} filepath: {complete_msg['filepath']}, \
+                user: {complete_msg['user']}, accessionid: {complete_msg['accession_id']}, \
+                decryptedChecksums: {complete_msg['decrypted_checksums']})",
+            )
+
+            ValidateJSON(load_schema("ingestion-completion")).validate(complete_msg)
+
+            # Send message to mappings queue for dataset to file mapping
+            accessionID = complete_msg["accession_id"]
+            datasetID = generate_dataset_id(complete_msg["user"], complete_msg["filepath"])
+            self._publish_mappings(message, accessionID, datasetID)
+
+        except ValidationError:
+            LOG.error("Could not validate the ingestion complete message. Not properly formatted.")
+            raise
+
+        except Exception as error:
+            LOG.error(f"Error occurred in complete consumer: {error}.")
+            raise
+
+    def _publish_mappings(self, message: Message, accessionID: str, datasetID: str) -> None:
+        """Publish message with dataset to accession ID mapping."""
+        properties = {
+            "content_type": "application/json",
+            "headers": {},
+            "correlation_id": message.correlation_id,
+            "delivery_mode": 2,
+        }
+        try:
 
             channel = self.connection.channel()  # type: ignore
-            datasetID = generate_dataset_id(complete_msg["user"], complete_msg["filepath"])
-            accessionID = complete_msg["accession_id"]
-            content = {
-                "type": "mapping",
-                "dataset_id": datasetID,
-                "accession_ids": [accessionID],
-            }
-            mapping = Message.create(channel, json.dumps(content), properties)
+            mappings_trigger = {"type": "mapping", "dataset_id": datasetID, "accession_ids": [accessionID]}
+
+            mappings_msg = json.dumps(mappings_trigger)
+            ValidateJSON(load_schema("dataset-mapping")).validate(json.loads(mappings_msg))
+
+            mapping = Message.create(channel, mappings_msg, properties)
             mapping.publish(
                 os.environ.get("MAPPINGS_QUEUE", "mappings"), exchange=os.environ.get("BROKER_EXCHANGE", "sda")
             )
 
             channel.close()
+
             LOG.info(
                 f"Sent the message to mappings queue to set dataset ID {datasetID} for file \
                      with accessionID {accessionID}."
             )
 
-        except Exception as error:
-            LOG.error("Something went wrong: {0}".format(error))
+        except ValidationError:
+            LOG.error("Could not validate the ingestion mappings message. Not properly formatted.")
+            raise Exception("Could not validate the ingestion mappings message. Not properly formatted.")
 
 
 def main() -> None:
