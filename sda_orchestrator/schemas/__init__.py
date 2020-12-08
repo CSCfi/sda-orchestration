4cd18e2fb1bf2e13e31eff82e1e234a30703f89f
Stefan Negru <stefan@blankdots.com>
2020-12-08 23:04:27 +0200

    add schemas license

--- sda_orchestrator/schemas/__init__.py
+++ sda_orchestrator/schemas/__init__.py
@@ -1,1 +1,5 @@
-"""JSON Schemas and function for validating messages."""
+"""JSON Schemas and function for validating messages.
+
+Schemas are provided by https://github.com/EGA-archive/LocalEGA/tree/master/ingestion/schemas
+Under Apache 2.0 license
+"""
