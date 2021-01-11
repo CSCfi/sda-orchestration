1d5721f88f399dce8683ce17e8693605c0945fde
Stefan Negru <stefan@blankdots.com>
2021-01-11 09:06:21 +0200

    configure DOI Handler for creating datacite dois
    We make use of httpx for creating requests
    
    modified the way datasets ids are generated we make use of shortuuid to generate ids that are human readable and usable. see https://blog.datacite.org/cool-dois/
    
    Configuration can be loaded externally as JSON

---
+++ sda_orchestrator/config/__init__.py
@@ -0,0 +1,22 @@
+"""SDA orchestrator configuration.
+
+Configuration required for DOIs, REMS and other metadata.
+"""
+
+from os import environ
+from pathlib import Path
+from typing import Dict
+import json
+
+
+def parse_config_file(config_file: str) -> Dict:
+    """Load JSON schemas."""
+    file_path = Path(config_file)
+
+    with open(str(file_path), "r") as fp:
+        return json.load(fp)
+
+
+CONFIG_INFO = parse_config_file(
+    environ.get("CONFIG_FILE", str(Path(__file__).resolve().parent.joinpath("config.json")))
+)
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.7",
     ],
-    install_requires=["amqpstorm", "jsonschema"],
+    install_requires=["amqpstorm", "jsonschema", "httpx", "shortuuid"],
     extras_require={
         "test": ["coverage", "coveralls", "pytest", "pytest-cov", "tox"],
     },
