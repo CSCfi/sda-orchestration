039b26939afb7513817c798f325861b41551ec05
Stefan Negru <stefan@blankdots.com>
2021-01-19 17:46:20 +0200

    raise filenot found if file does not exist

--- sda_orchestrator/config/__init__.py
+++ sda_orchestrator/config/__init__.py
@@ -3,16 +3,21 @@
 Configuration required for DOIs, REMS and other metadata.
 """
 
-from os import environ
+from os import environ, strerror
 from pathlib import Path
 from typing import Dict
 import json
+import errno
+
+from ..utils.logger import LOG
 
 
 def parse_config_file(config_file: str) -> Dict:
     """Load JSON schemas."""
     file_path = Path(config_file)
-
+    if not file_path.is_file():
+        LOG.error(f"File {file_path} not found")
+        raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), file_path)
     with open(file_path, "r") as fp:
         return json.load(fp)
 
