014449dba6bd1bfb6838d81b06836eef315ad11c
Stefan Negru <stefan@blankdots.com>
2021-09-29 19:17:26 +0300

    bump version to 0.7.0

--- sda_orchestrator/__init__.py
+++ sda_orchestrator/__init__.py
@@ -1,5 +1,5 @@
 """SDA Orchestrator service for coordinating messages and mapping file id to dataset id."""
 
 __title__ = "sda_orchestrator"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __author__ = "NeIC System Developers"
